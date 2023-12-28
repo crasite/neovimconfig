b031c473028b3e3ae29756c9ffde6838e6cb1ebb
Thanu Poptiphueng <tpcrasite@gmail.com>
2023-12-28 10:37:26 +0700

    make typescript look like it's uncommented

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,17 +12,16 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
     -- import/override with your plugins
     --
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "plugins" },
     { "wuelnerdotexe/vim-astro" },
-    {"google/vim-jsonnet"}
+    { "google/vim-jsonnet" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
